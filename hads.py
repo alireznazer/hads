47e22cef37d36cfb6bd85d08dcde62c690a9e659
alireza.siro <135672653+alireznazer@users.noreply.github.com>
2023-06-09 02:14:40 +0330

    Add files via upload

---
+++ hads.py
@@ -0,0 +1,18 @@
+import random
+entekhabsystem = random.randint(1,100)
+while (True):
+
+    print("Entekhb = 1 ta 100")
+    yourentehekhab = int(input("Enter your number:"))
+    if yourentehekhab > entekhabsystem:
+        print("your entehekhab > entekhab system")
+       
+    elif yourentehekhab < entekhabsystem:
+        print("your entehekhab < entekhab system")
+       
+    elif yourentehekhab == entekhabsystem:
+        print("hadese is True")
+        print(entekhabsystem)
+        print("\n\n")
+    print("\n")
+     
